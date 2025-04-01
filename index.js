503c40756d649164d9d8d216b183c7817ce5a281
julienpowellHD <julien.f.powell@hypeanddexter.com>
2025-04-02 12:01:24 +1300

    quickly throw together 3 routes

--- index.js
+++ index.js
@@ -13,14 +13,67 @@ const PRIVATE_APP_ACCESS = '';
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const customObjects = 'https://api.hubspot.com/crm/v3/objects/0-2';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(customObjects, { headers });
+        const data = resp.data.results;
+        res.render('index', { title: 'Homepage | Custom Objects', data });
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+    try {
+        res.render('form', { 
+            title: 'Create/Update Custom Object',
+            data: req.query.id ? {
+                id: req.query.id,
+                ...req.query
+            } : null
+        });
+    } catch (error) {
+        console.error(error);
+    }
+});
+
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const customObjects = 'https://api.hubspot.com/crm/v3/objects/0-2';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    const update = {
+        properties: req.body
+    };
+
+    try {
+        if (req.body.id) {
+            // Update existing object
+            await axios.patch(`${customObjects}/${req.body.id}`, update, { headers });
+        } else {
+            // Create new object
+            await axios.post(customObjects, update, { headers });
+        }
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error processing request');
+    }
+});
+
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
